5c94da8ceb3737f4bb624b29ccbb0296ada08512
Sh4rk0 <francesco.raimondo@gmail.com>
2020-11-08 13:11:34 +0100

    added bonus, scene transition, joypad support...

--- public/sw.js
+++
@@ -1,4 +0,0 @@
-importScripts("precache-manifest.da81b8e8a338fbc6e2b863a38eff65e6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-workbox.precaching.precacheAndRoute(__precacheManifest);
-
